b25f01e6ffbd2f4b4c26d4813f471106855d164c
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2024-05-28 13:58:18 +0800

    add sunburst.py
    containing a plotly visualization

---
+++ pages/sunburst.py
@@ -0,0 +1,52 @@
+import dash
+import pandas as pd
+import plotly.express as px
+import dash_bootstrap_components as dbc
+from dash import Dash, dcc, callback, html, Input, Output
+
+df = pd.read_csv('./datas/sunburst.csv')
+
+dash.register_page(__name__)
+
+layout = html.Div([
+    html.Div([
+        dcc.Dropdown(
+                id='year-dropdown',
+                options=[{'label': year, 'value': year} for year in df['學年度'].unique()],
+                value=df['學年度'].unique()[0],
+                clearable=False,
+                style={
+                    'marginBottom':"8px",
+                }
+            ),
+            dcc.Graph(id="sunburst-chart", style={
+                'height':"450px",}),
+            
+            dcc.Link(html.Button("Home",
+                             style={
+                                 'color':"purple"
+                                 }), href="/", refresh=True),
+        
+    ])
+])
+
+@callback(
+    Output("sunburst-chart", "figure"), 
+    [Input("year-dropdown", "value")]
+)
+
+
+def AcademicYearChart(selected_year):
+    filtered_df = df[df['學年度'] == selected_year]
+    student_counts = filtered_df.groupby(['學制', '學制/系科']).size().reset_index(name='Count')
+    fig = px.sunburst(student_counts, path=['學制', '學制/系科'], values='Count',
+                      title=f'{selected_year} 學年學生分佈'
+                    #   title=f'Student Distribution for Academic Year {selected_year}' 
+                      )
+    fig.update_layout(
+        margin = dict(t=25, l=25, r=25, b=10)
+    )
+    return fig
+
+
+
